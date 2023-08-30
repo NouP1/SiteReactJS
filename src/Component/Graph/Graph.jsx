bd730aa7dc1aadd93b2ca1d49812577aa0312e2c
NouP1 <smolen43ret@gmail.com>
2023-08-31 01:24:57 +0500

    Commit

--- src/Component/Graph/Graph.jsx
+++ src/Component/Graph/Graph.jsx
@@ -17,13 +17,25 @@ import './../Graph/Graph.css'
                                 <div className="line_1"></div>
                             
                         </div>
-                        <div className="Columns">
+                        
+                            <div className="Columns">
+                            <div className={show ? 'Columns1': 'Columns'}>  
                                 <div className='Column'> <span className='persent_1'> 6% </span><span className={show ?'persent1_2':'persent1'}>1% </span> </div>
-                                <div className='Column'> <span className='persent_1'> 6% </span><span className="persent2">2% </span> </div>
+                                <div className='Column'> <span className='persent_1'> 6% </span><span className={show ? "persent2_1":'persent2'}>2% </span> </div>
                                 <div className='Column'> <span className='p_'> 6% </span><span className="persent3">4% </span> </div>
-                        </div>
-                        <div className="graph_wrapper_linev"></div>
-                        <div className="graph_wrapper_lineh"></div>
+                           </div>
+                            <div className='Columns1'>
+                            <div className={show ? 'Columns': 'Columns1'}>   
+                                <div className='Column'> <span className='persent_1'> 15% </span><span className={show ?'persent1_2':'persent1'}>5% </span> </div>
+                                <div className='Column'> <span className='persent_1'> 15% </span><span className={show ? "persent2_1":'persent2'}>7% </span> </div>
+                                <div className='Column'> <span className='p_'> 15% </span><span className="persent3">10% </span> </div>
+                            </div> 
+                            </div> 
+                       
+                      </div>  <div className="graph_wrapper_linev"></div>
+                        <div className="graph_wrapper_lineh">
+                            <div className='Years'> 1год 2год 3год </div>
+                        </div>   
                     </div>
                     
                   
--- src/Component/Ngk/Ngk.jsx
+++ src/Component/Ngk/Ngk.jsx
@@ -1,18 +1,21 @@
 import './../Ngk/Ngk.css'
 function Ngk() {
     return <ngk>
+                <div className='container'>
                 <div className='NGK'>Налоговые каникулы</div>
                     <div className='str'> А можно вообще без налога?<br/><span className='orange'>Да!0% для ИП на 2 года</span></div>
             
                 <div className='Block_1_'> <span className='bl1'>Вид налогообложения</span><span className='bl2'> —Упрощённая система <br/> налогообложения (УСН)<br/>— Патентная система <br/> налогообложения (ПСН)</span>
                 <div className='Block_2_'> <span className='bl1_1'>Вид деятельности</span><p></p><span className='bl'> — сельское хозяйство <br/>— обрабатывающие производства<br/>— научные исследования и разработки<br/>— образование<br/>— здравоохранение<br/>— социальные услуги<br/>— ремонт компьютеров и бытовых предметов<br/>— гостиничный бизнес*<br/>— IT*</span>—
-
-                    
-                    </div>
+                <div className='Block_3_'><span className='bl3_1'>Основания</span><br/><span className='bl3_2'>Налоговые каникулы предоставляются ИП:<br/><br/>— по УСН на основании Закона<br/>Пермского края от 01.04.2015<br/>(№ 466-ПК)</span>
+                <span className='bl3_3'>— по ПСН на основании Закона<br/>Пермского края от 01.04.2015<br/>(№ 465-ПК)</span></div>
+                
+                </div>
+                    <div className='bl3_4'>*только на УСН</div>
                 </div>
                 
 
-
+            </div>
             </ngk>
 
     
