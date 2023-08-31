52939cc7196c5966c19706ffa43027ba9a89f819
Noup1 <smolen43ret@gmail.com>
2023-08-31 13:58:57 +0300

    graph_finishing

--- src/Component/Graph/Graph.jsx
+++ src/Component/Graph/Graph.jsx
@@ -3,6 +3,26 @@ import './../Graph/Graph.css'
  function Graph () {
     const [show,setshow] = useState(false)
 
+    let element = document.getElementById('Columns1');
+    if (element) {
+    element.style.visibility = show ? 'visible' : 'hidden';
+    } 
+
+
+    /*function onEntry(entry) {
+        entry.forEach(change => {
+          if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+          }
+        });
+      }
+      let options = { threshold: [0.5] };
+      let observer = new IntersectionObserver(onEntry, options);
+      let elements = document.querySelectorAll('.Table');
+      for (let elm of elements) {
+        observer.observe(elm);
+      }*/
+
 
     return <graph>
             <div class="Container_2">   
@@ -18,21 +38,18 @@ import './../Graph/Graph.css'
                             
                         </div>
                         
-                            <div className="Columns">
-                            <div className={show ? 'Columns1': 'Columns'}>  
+                            <div className='Columns'>
                                 <div className='Column'> <span className='persent_1'> 6% </span><span className={show ?'persent1_2':'persent1'}>1% </span> </div>
                                 <div className='Column'> <span className='persent_1'> 6% </span><span className={show ? "persent2_1":'persent2'}>2% </span> </div>
                                 <div className='Column'> <span className='p_'> 6% </span><span className="persent3">4% </span> </div>
-                           </div>
-                            <div className='Columns1'>
-                            <div className={show ? 'Columns': 'Columns1'}>   
+                            </div>
+                            <div className='Columns1' id='Columns1'>  
                                 <div className='Column'> <span className='persent_1'> 15% </span><span className={show ?'persent1_2':'persent1'}>5% </span> </div>
                                 <div className='Column'> <span className='persent_1'> 15% </span><span className={show ? "persent2_1":'persent2'}>7% </span> </div>
                                 <div className='Column'> <span className='p_'> 15% </span><span className="persent3">10% </span> </div>
                             </div> 
-                            </div> 
                        
-                      </div>  <div className="graph_wrapper_linev"></div>
+                        <div className="graph_wrapper_linev"></div>
                         <div className="graph_wrapper_lineh">
                             <div className='Years'> 1год 2год 3год </div>
                         </div>   
